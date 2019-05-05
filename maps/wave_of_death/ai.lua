f8151d56791d1bca87137d3c53f1614888e8dbdb
MewMew <mail@mewmew.net>
2019-05-06 01:43:10 +0200

    WoD wip

--- maps/wave_of_death/ai.lua
+++ maps/wave_of_death/ai.lua
@@ -1,46 +1,49 @@
 local biter_waves = require "maps.wave_of_death.biter_waves"
 local ai = {}
 
-ai.send_wave_command = function(lane_number)
-	local surface = game.surfaces["nauvis"]
-	for _, unit in pairs(global.wod_biters) do
-		if unit.entity.valid then
-			if unit.lane_number == lane_number then
-				unit.entity.set_command({
-					type = defines.command.attack_area,
-					destination = global.loaders[lane_number].position,
-					radius = 32,
-					distraction = defines.distraction.by_anything,
-				})
-			end
-		end
-	end
-end
-
-ai.spawn_wave = function(surface, lane_number, amount_modifier)
-	local wave_number = global.wod_lane[lane_number].current_wave
-	if not biter_waves[wave_number] then wave_number = #biter_waves end
-
+ai.spawn_wave = function(surface, lane_number, wave_number, amount_modifier)	
 	local is_spread_wave = true
 	if amount_modifier == 1 then is_spread_wave = false end
 	
 	if not global.loaders[lane_number].valid then return end
 	
-	local spawn_position = {x = global.loaders[lane_number].position.x, y = global.loaders[lane_number].position.y - 96}
+	local spawn_position = {x = global.loaders[lane_number].position.x, y = global.loaders[lane_number].position.y - 288}
+	
+	local unit_group = surface.create_unit_group({position = spawn_position, force = "enemy"})
 	
 	for _, biter_type in pairs(biter_waves[wave_number]) do
 		for count = 1, math.floor(biter_type.amount * amount_modifier), 1 do
-			local pos = surface.find_non_colliding_position(biter_type.name, spawn_position, 96, 2)
+			local pos = surface.find_non_colliding_position(biter_type.name, spawn_position, 96, 3)
 			local biter = surface.create_entity({name = biter_type.name, position = pos, force = "enemy"})
 			global.wod_biters[biter.unit_number] = {entity = biter, lane_number = lane_number, spread_wave = is_spread_wave}
+			unit_group.add_member(biter)
 			if not is_spread_wave then
 				global.wod_lane[lane_number].alive_biters = global.wod_lane[lane_number].alive_biters + 1
 			end
 		end
 	end
-
+	
+	unit_group.set_command({
+		type = defines.command.compound,
+		structure_type = defines.compound_command.return_last,
+		commands = {
+			{
+				type = defines.command.attack_area,
+				destination = global.loaders[lane_number].position,
+				radius = 32,
+				distraction=defines.distraction.by_enemy
+			},									
+			{
+				type = defines.command.attack,
+				target = global.loaders[lane_number],
+				distraction = defines.distraction.by_enemy
+			}
+		}
+	})
+	
 	if is_spread_wave then return end
 	global.wod_lane[lane_number].current_wave = global.wod_lane[lane_number].current_wave + 1
+	global.biter_evasion_health_increase_factor = global.biter_evasion_health_increase_factor + 0.005
 end
 
 --on_entity_died event
@@ -57,12 +60,11 @@ ai.spawn_spread_wave = function(event)
 
 	for lane_number = 1, 4, 1 do
 		if lane_number ~= trigger_lane_number then
-			ai.spawn_wave(entity.surface, lane_number, global.spread_amount_modifier)
-			ai.send_wave_command(lane_number)
+			ai.spawn_wave(entity.surface, lane_number, global.wod_lane[trigger_lane_number].current_wave - 1, global.spread_amount_modifier)	
 		end
 	end
 
-	game.print("Lane #" .. trigger_lane_number .. " has spread biters.")
+	game.print("Lane #" .. trigger_lane_number .. " has defeated their wave.")
 end
 
 --on_entity_rotated event
@@ -76,13 +78,14 @@ ai.trigger_new_wave = function(event)
 		return 
 	end
 
-    ai.spawn_wave(entity.surface, lane_number, 1)
-	ai.send_wave_command(lane_number)
+	local wave_number = global.wod_lane[lane_number].current_wave
+	if not biter_waves[wave_number] then wave_number = #biter_waves end
+    ai.spawn_wave(entity.surface, lane_number, wave_number, 1)
 	
 	for _, player in pairs(game.connected_players) do
 		player.play_sound{path="utility/new_objective", volume_modifier=0.3} --dont know if a sound would be annoying in a game with 10 lanes playing ^^ maybe a short sound
 	end
-	game.print("Lane #" .. entity.force.name .. " has summoned wave ")	
+	game.print("Lane " .. entity.force.name .. " has summoned wave #" .. global.wod_lane[lane_number].current_wave - 1)	
 end
 
 return ai
