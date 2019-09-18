e03750e6b607fb3e43b14b09070af9c822b52415
MaemiKozue <51482990+MaemiKozue@users.noreply.github.com>
2019-09-18 21:56:43 +0200

    Biter Battles chunk processing optimization

--- maps/biter_battles_v2/config.lua
+++ maps/biter_battles_v2/config.lua
@@ -4,28 +4,28 @@ bb_config = {
 	--TEAM SETTINGS--
 	["team_balancing"] = true,			--Should players only be able to join a team that has less or equal members than the opposing team?
 	["only_admins_vote"] = false,		--Are only admins able to vote on the global difficulty?
-	
+
 	--Optional custom team names, can also be modified via "Team Manager"
-	["north_side_team_name"] = "Team North",		
-	["south_side_team_name"] = "Team South",	
+	["north_side_team_name"] = "Team North",
+	["south_side_team_name"] = "Team South",
 
 	--GENERAL SETTINGS--
 	["blueprint_library_importing"] = false,		--Allow the importing of blueprints from the blueprint library?
 	["blueprint_string_importing"] = false,		--Allow the importing of blueprints via blueprint strings?
 
 	--MAP PREGENERATION--
-	["map_pregeneration_radius"] = 26,	 		--3 horizontal radiuses in chunks to pregenerate at the start of the map.
-	["on_init_pregen"] = true,	 					--Generate some chunks on_init?
+	["map_pregeneration_radius"] = 0,	 		--3 horizontal radiuses in chunks to pregenerate at the start of the map.
+	["on_init_pregen"] = false,	 					--Generate some chunks on_init?
 	["fast_pregen"] = false,	 						--Force fast pregeneration.
-	
+
 	--TERRAIN OPTIONS--
 	["border_river_width"] = 29,						--Approximate width of the horizontal impassable river seperating the teams. (values up to 100)
 	["builders_area"] = true,							--Grant each side a peaceful direction with no nests and biters?
 	["random_scrap"] = false,							--Generate harvestable scrap around worms randomly?
-	
+
 	--BITER SETTINGS--
 	["max_active_biters"] = 2500,					--Maximum total amount of attacking units per side.
 	["max_group_size"] = 256,						--Maximum unit group size.
-	["biter_timeout"] = 54000,						--Time it takes in ticks for an attacking unit to be deleted. This prevents perma stuck units.	
+	["biter_timeout"] = 54000,						--Time it takes in ticks for an attacking unit to be deleted. This prevents perma stuck units.
 	["bitera_area_distance"] = 416					--Distance to the biter area.
 }
--- maps/biter_battles_v2/on_tick.lua
+++ maps/biter_battles_v2/on_tick.lua
@@ -3,35 +3,35 @@ local event = require 'utils.event'
 local gui = require "maps.biter_battles_v2.gui"
 local ai = require "maps.biter_battles_v2.ai"
 local chunk_pregen = require "maps.biter_battles_v2.pregenerate_chunks"
-local mirror_map = require "maps.biter_battles_v2.mirror_terrain"
+local mirror_tick_routine = require "maps.biter_battles_v2.mirror_terrain"
 local server_restart = require "maps.biter_battles_v2.game_won"
 
 local spy_forces = {{"north", "south"},{"south", "north"}}
 local function spy_fish()
-	for _, f in pairs(spy_forces) do		
+	for _, f in pairs(spy_forces) do
 		if global.spy_fish_timeout[f[1]] - game.tick > 0 then
 			local r = 96
 			local surface = game.surfaces["biter_battles"]
 			for _, player in pairs(game.forces[f[2]].connected_players) do
 				game.forces[f[1]].chart(surface, {{player.position.x - r, player.position.y - r}, {player.position.x + r, player.position.y + r}})
-			end			
+			end
 		else
 			global.spy_fish_timeout[f[1]] = 0
 		end
-	end		
+	end
 end
 
 local function reveal_map()
-	for _, f in pairs({"north", "south", "player", "spectator"}) do			
-		local r = 768		
-		game.forces[f].chart(game.surfaces["biter_battles"], {{r * -1, r * -1}, {r, r}})		
-	end		
+	for _, f in pairs({"north", "south", "player", "spectator"}) do
+		local r = 768
+		game.forces[f].chart(game.surfaces["biter_battles"], {{r * -1, r * -1}, {r, r}})
+	end
 end
 
 local function clear_corpses()
 	local corpses = game.surfaces["biter_battles"].find_entities_filtered({type = "corpse"})
 	if #corpses < 1024 then return end
-	for _, e in pairs(corpses) do		
+	for _, e in pairs(corpses) do
 		if math.random(1, 3) == 1 then
 			e.destroy()
 		end
@@ -48,31 +48,31 @@ local function restart_idle_map()
 end
 
 local function on_tick(event)
+	mirror_tick_routine()
 	if game.tick % 30 ~= 0 then return end
 	chunk_pregen()
-	mirror_map()
-	
+
 	if game.tick % 60 ~= 0 then return end
 	global.bb_threat["north_biters"] = global.bb_threat["north_biters"] + global.bb_threat_income["north_biters"]
 	global.bb_threat["south_biters"] = global.bb_threat["south_biters"] + global.bb_threat_income["south_biters"]
-	
-	if game.tick % 180 == 0 then gui() end	
-	
+
+	if game.tick % 180 == 0 then gui() end
+
 	if game.tick % 300 ~= 0 then return end
 	spy_fish()
 	if global.bb_game_won_by_team then
 		reveal_map()
-		server_restart() 
-		return 
+		server_restart()
+		return
 	end
-	
+
 	if game.tick % 3600 ~= 0 then return end
 	ai.raise_evo()
 	ai.destroy_inactive_biters()
 	ai.main_attack()
 	ai.send_near_biters_to_silo()
 	ai.destroy_old_age_biters()
-	
+
 	clear_corpses()
 	restart_idle_map()
 end
