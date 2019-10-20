24ad7d0d26bd4f963c11b3bfe61f9ce907fbdff9
Gerkiz <notify@nvfs.se>
2019-10-20 18:51:48 +0200

    moved poll after score

--- control.lua
+++ control.lua
@@ -16,9 +16,9 @@ require "player_modifiers"
 
 require "comfy_panel.main"
 require "comfy_panel.player_list"
-require "comfy_panel.poll"
 require "comfy_panel.group"
 require "comfy_panel.score"
+require "comfy_panel.poll"
 require "comfy_panel.admin"
 
 require "modules.autostash"
