c370333c78e3f443b76f0c5a0934f2f2768b6e23
MewMew <mail@mewmew.net>
2019-11-09 14:14:21 +0100

    small tweak

--- modules/biter_noms_you.lua
+++ modules/biter_noms_you.lua
@@ -12,6 +12,7 @@ local strings = {
 	"nibble", "nibble",
 	"nom", "nom",  "nom",  "nom",  "nom",  "nom",  "nom",  "nom",  "nom",  "nom",  "nom", "nom", "nom", "nom", "nom", "nom", "nom", "nom", "nom", "nom", "nom", "nom"
 }
+local size_of_strings = #strings
 
 local whitelist = {
 	["small-biter"] = true,
@@ -24,11 +25,11 @@ local function on_entity_damaged(event)
 	if not event.cause then return end
 	if not event.cause.valid then return end
 	if not whitelist[event.cause.name] then return end
-	if math.random(1,5) == 1 then
+	if math_random(1,5) == 1 then
 		event.cause.surface.create_entity({
 			name = "flying-text",
 			position = event.cause.position,
-			text = strings[math_random(1,#strings)],
+			text = strings[math_random(1, size_of_strings)],
 			color = {r = math_random(130, 170), g = math_random(130, 170), b = 130}
 		})
 	end
