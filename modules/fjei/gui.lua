9a1f1f46a87c8c7c4c779dc258bb8de6ba7c29bb
MewMew <mail@mewmew.net>
2019-11-11 16:53:00 +0100

    WIP

--- modules/fjei/gui.lua
+++ modules/fjei/gui.lua
@@ -1,3 +1,4 @@
+local math_ceil = math.ceil
 local height = 600
 local width = 400
 local column_count = 8
@@ -5,22 +6,104 @@ local line_count = 12
 local items_per_page = column_count * line_count
 local Public = {}
 
-local function draw_main_window(player)
-	local item_list = global.fjei.item_list
+local function get_total_page_count(player)
+	return math_ceil(global.fjei.player_data[player.index].size_of_filtered_list / items_per_page)
+end
 
+local function set_page_count_caption(player)
+	if not player.gui.left.fjei_main_window then return end
+	local active_page = global.fjei.player_data[player.index].active_page
+	local element = player.gui.left.fjei_main_window.fjei_main_window_control_table.fjei_main_window_page_counter
+	element.caption = "Page " .. active_page .. " of " .. get_total_page_count(player)
+end
+
+local function display_item_list(player)
+	if not player.gui.left.fjei_main_window then return end
+	if not player.gui.left.fjei_main_window.fjei_main_window_item_list_table then return end
+	local item_list_table = player.gui.left.fjei_main_window.fjei_main_window_item_list_table
+	item_list_table.clear()
+	
+	local active_page = global.fjei.player_data[player.index].active_page
+	local starting_index = 1 + (active_page - 1) * items_per_page
+	local item_list = global.fjei.item_list
+	local filtered_list = global.fjei.player_data[player.index].filtered_list	
+	
+	for i = starting_index, starting_index + items_per_page - 1, 1 do
+		if not filtered_list[i] then return end
+		local item_key = filtered_list[i]
+		if not item_list[item_key] then return end		
+		local sprite = item_list_table.add({type = "sprite", sprite = "recipe/" .. item_list[item_key].name, tooltip = item_list[item_key].name})
+		sprite.style.maximal_width = 32
+		sprite.style.maximal_height = 32
+		sprite.style.margin = 4		
+	end
+end
+
+function Public.refresh_main_window(player)
+	set_page_count_caption(player)
+	display_item_list(player)
+end
+
+local function draw_main_window(player)
 	if player.gui.left["fjei_main_window"] then player.gui.left["fjei_main_window"].destroy() end
-	local frame = player.gui.left.add({type = "frame", name = "fjei_main_window", caption = "search"})
+	local frame = player.gui.left.add({type = "frame", name = "fjei_main_window", direction = "vertical"})
 	frame.style.minimal_height = height
 	frame.style.minimal_width = width
 	frame.style.padding = 2
 	
-	local t = frame.add({type = "table", name = "fjei_main_window_table", column_count = column_count})
-	for i = 1, items_per_page, 1 do
-		local sprite = t.add({type = "sprite", sprite = item_list[i].sprite, tooltip = item_list[i].name})
-		sprite.style.maximal_width = 28
-		sprite.style.maximal_height = 28
-		sprite.style.margin = 4
+	local t = frame.add({type = "table", name = "fjei_main_window_control_table",  column_count = 4})
+	local element = t.add({type = "label", name = "fjei_main_window_page_counter",  caption = " "})
+	element.style.font = "heading-1"
+	element.style.margin = 4
+	
+	local element = t.add({type = "sprite-button", name = "fjei_main_window_previous_page", caption = "←"})
+	element.style.font = "heading-1"
+	element.style.maximal_height = 38
+	element.style.maximal_width = 38
+	local element = t.add({type = "sprite-button", name = "fjei_main_window_next_page", caption = "→"})
+	element.style.font = "heading-1"
+	element.style.maximal_height = 38
+	element.style.maximal_width = 38
+	local textfield = t.add({ type = "textfield", name = "fjei_main_window_search_textfield", text = "" })
+	textfield.style.minimal_width = 160	
+	
+	frame.add({type = "line"})
+		
+	local t = frame.add({type = "table", name = "fjei_main_window_item_list_table", column_count = column_count})
+	
+	Public.refresh_main_window(player)
+end
+
+local function toggle_main_window(element, player)
+	if player.gui.left.fjei_main_window then
+		player.gui.left.fjei_main_window.destroy()		
+	else
+		draw_main_window(player)	
 	end
+	return true
+end
+
+local function main_window_next_page(element, player)
+	if global.fjei.player_data[player.index].active_page == get_total_page_count(player) then return end
+	global.fjei.player_data[player.index].active_page = global.fjei.player_data[player.index].active_page + 1
+	Public.refresh_main_window(player)
+end
+
+local function main_window_previous_page(element, player)
+	if global.fjei.player_data[player.index].active_page == 1 then return end
+	global.fjei.player_data[player.index].active_page = global.fjei.player_data[player.index].active_page - 1
+	Public.refresh_main_window(player)
+end
+
+local gui_actions = {
+	["fjei_toggle_button"] = toggle_main_window,
+	["fjei_main_window_next_page"] = main_window_next_page,
+	["fjei_main_window_previous_page"] = main_window_previous_page,
+}
+
+function Public.gui_click_actions(element, player)
+	if not gui_actions[element.name] then return end
+	gui_actions[element.name](element, player)
 end
 
 function Public.draw_top_toggle_button(player)
@@ -29,16 +112,6 @@ function Public.draw_top_toggle_button(player)
 	button.style.minimal_height = 38
 	button.style.minimal_width = 38
 	button.style.padding = -2
-end
-
-function Public.toggle_main_window(element, player)	
-	if element.name ~= "fjei_toggle_button" then return end
-	if player.gui.left.fjei_main_window then
-		player.gui.left.fjei_main_window.destroy()		
-	else
-		draw_main_window(player)	
-	end
-	return true
 end
 
 return Public
--- modules/fjei/main.lua
+++ modules/fjei/main.lua
@@ -1,42 +1,90 @@
 --[[
 FJEI - Factorio "Just enough items"
-An item recipe browser
+An item recipe browser - MewMew
 ]]
 
 local Gui = require "modules.fjei.gui"
 
-local function set_item_list()
+local function set_base_item_list()
 	global.fjei.item_list = {}
 	local list = global.fjei.item_list
 	local i = 1
 	for name, prototype in pairs(game.recipe_prototypes) do	
-		list[i] = {name = name, sprite = "recipe/" .. name}
+		list[i] = {name = name}
 		i = i + 1
 	end
 	table.sort(list, function (a, b) return a.name < b.name end)
+	global.fjei.size_of_item_list = #list
+end
+
+local function set_filtered_list(player)
+	local player_data = global.fjei.player_data[player.index]
+	local active_filter = player_data.active_filter
+	local base_list = global.fjei.item_list
+	player_data.active_page = 1
+	player_data.filtered_list = {}
+	local filtered_list = player_data.filtered_list
+	local i = 1
+	for key, entry in pairs(base_list) do
+		if active_filter then
+			local a, b = string.find(entry.name, active_filter)
+			if a then
+				filtered_list[i] = key
+				i = i + 1
+			end
+		else
+			filtered_list[i] = key
+			i = i + 1
+		end
+	end
+	player_data.size_of_filtered_list = #player_data.filtered_list
 end
 
 local function on_player_joined_game(event)
 	local player = game.players[event.player_index]
-	set_item_list()
+	global.fjei.player_data[player.index] = {}
+	set_base_item_list()
+	set_filtered_list(player)
 	Gui.draw_top_toggle_button(player)
+end
+
+local function on_player_left_game(event)
+	local player = game.players[event.player_index]
+	global.fjei.player_data[player.index].filtered_list = nil
+	global.fjei.player_data[player.index] = nil
 end
 
 local function on_gui_click(event)
 	local element = event.element
 	if not element then return end
 	if not element.valid then return end
-	local player = game.players[event.player_index]
-	
-	if Gui.toggle_main_window(element, player) then return end
+	local player = game.players[event.player_index]	
+	Gui.gui_click_actions(element, player)
+end
 
+local function on_gui_text_changed(event)
+	local element = event.element
+	if not element then return end
+	if not element.valid then return end
+	if element.name ~= "fjei_main_window_search_textfield" then return end
+	local player = game.players[event.player_index]
+	if element.text == "" then
+		global.fjei.player_data[player.index].active_filter = false
+	else
+		global.fjei.player_data[player.index].active_filter = element.text
+	end
+	set_filtered_list(player)
+	Gui.refresh_main_window(player)
 end
 
 local function on_init()
 	global.fjei = {}
+	global.fjei.player_data = {}
 end
 
 local event = require "utils.event"
 event.add(defines.events.on_player_joined_game, on_player_joined_game)
+event.add(defines.events.on_player_left_game, on_player_left_game)
 event.add(defines.events.on_gui_click, on_gui_click)
+event.add(defines.events.on_gui_text_changed, on_gui_text_changed)
 event.on_init(on_init)
