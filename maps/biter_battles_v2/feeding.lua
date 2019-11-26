2411848b34a4c89d5a3ddf6a1fb7dafc7834b47b
MewMew <mail@mewmew.net>
2019-11-26 05:24:51 +0100

    threat income tweak

--- maps/biter_battles_v2/feeding.lua
+++ maps/biter_battles_v2/feeding.lua
@@ -86,7 +86,7 @@ local function print_feeding_msg(player, food, flask_amount)
 end
 
 function set_evo_and_threat(flask_amount, food, biter_force_name)
-	local decimals = 12
+	local decimals = 9
 	local math_round = math.round
 	
 	local instant_threat_player_count_modifier = get_instant_threat_player_count_modifier()
@@ -94,12 +94,6 @@ function set_evo_and_threat(flask_amount, food, biter_force_name)
 	local food_value = food_values[food].value * global.difficulty_vote_value
 	
 	for a = 1, flask_amount, 1 do				
-		--SET THREAT INCOME
-		local e = (global.bb_evolution[biter_force_name] * 100) + 1
-		local diminishing_modifier = (1 / (10 ^ (e * 0.014))) / (e * 0.5)
-		global.bb_threat_income[biter_force_name] = global.bb_threat_income[biter_force_name] + (food_value * diminishing_modifier * 12)		
-		global.bb_threat_income[biter_force_name] = math_round(global.bb_threat_income[biter_force_name], decimals)
-		
 		---SET EVOLUTION
 		local e2 = (game.forces[biter_force_name].evolution_factor * 100) + 1
 		local diminishing_modifier = (1 / (10 ^ (e2 * 0.017))) / (e2 * 0.5)
@@ -111,12 +105,15 @@ function set_evo_and_threat(flask_amount, food, biter_force_name)
 		else
 			game.forces[biter_force_name].evolution_factor = 1
 		end
-						
+		
 		--ADD INSTANT THREAT
 		local diminishing_modifier = 1 / (0.2 + (e2 * 0.018))
 		global.bb_threat[biter_force_name] = global.bb_threat[biter_force_name] + (food_value * instant_threat_player_count_modifier * diminishing_modifier)
-		global.bb_threat[biter_force_name] = math_round(global.bb_threat[biter_force_name], decimals)
+		global.bb_threat[biter_force_name] = math_round(global.bb_threat[biter_force_name], decimals)		
 	end
+	
+	--SET THREAT INCOME
+	global.bb_threat_income[biter_force_name] = global.bb_evolution[biter_force_name] * 20
 	
 	set_biter_endgame_modifiers(game.forces[biter_force_name])
 end
