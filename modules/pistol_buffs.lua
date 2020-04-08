12f826762a933bb18646481e7fed873711986abc
hanakocz <jujky.zon@gmail.com>
2020-04-08 21:31:34 +0200

    debug messages out

--- modules/pistol_buffs.lua
+++ modules/pistol_buffs.lua
@@ -12,8 +12,6 @@ local function on_entity_damaged(event)
 	if player.shooting_state.state == defines.shooting.not_shooting then return end
 	local weapon = player.get_inventory(defines.inventory.character_guns)[player.selected_gun_index]
 	local ammo = player.get_inventory(defines.inventory.character_ammo)[player.selected_gun_index]
-  game.print(weapon.valid_for_read)
-  game.print(ammo.valid_for_read)
   if not weapon.valid_for_read or not ammo.valid_for_read then return end
 	if weapon.name ~= "pistol" then return end
 	if ammo.name ~= "firearm-magazine" and ammo ~= "piercing-rounds-magazine" and ammo ~= "uranium-rounds-magazine" then return end
