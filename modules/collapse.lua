17d2144b7159f830517cbc946167d5d6e99e8972
MewMew <mail@mewmew.net>
2020-04-29 14:25:03 +0200

    update

--- modules/collapse.lua
+++ modules/collapse.lua
@@ -53,7 +53,7 @@ local function set_collapse_tiles()
 	if not collapse.tiles then return end
 	collapse.size_of_tiles = #collapse.tiles
 	if collapse.size_of_tiles > 0 then table_shuffle_table(collapse.tiles) end
-	collapse.current_position = {x = collapse.current_position.x + collapse.vector[1], y = collapse.current_position.y + collapse.vector[2]}
+	collapse.position = {x = collapse.position.x + collapse.vector[1], y = collapse.position.y + collapse.vector[2]}
 	local v = collapse.vector
 	local area = collapse.area
 	collapse.area = {{area[1][1] + v[1], area[1][2] + v[2]}, {area[2][1] + v[1], area[2][2] + v[2]}}
@@ -72,9 +72,11 @@ local function progress()
 		local tile = tiles[collapse.size_of_tiles]
 		if not tile then collapse.tiles = nil return end
 		collapse.size_of_tiles = collapse.size_of_tiles - 1
-		local position = {tile.position.x + 0.5, tile.position.y + 0.5}
-		for _, e in pairs(surface.find_entities_filtered({area = {{position[1] - 2, position[2] - 2}, {position[1] + 2, position[2] + 2}}})) do
-			if e.valid and e.health then e.die() end
+		if collapse.kill then
+			local position = {tile.position.x + 0.5, tile.position.y + 0.5}
+			for _, e in pairs(surface.find_entities_filtered({area = {{position[1] - 2, position[2] - 2}, {position[1] + 2, position[2] + 2}}})) do
+				if e.valid and e.health then e.die() end
+			end
 		end
 		surface.set_tiles({{name = "out-of-map", position = tile.position}}, true)
 	end
@@ -89,13 +91,13 @@ end
 
 function Public.set_direction(direction)
 	if not directions[direction] then print_debug(11) return end
-	directions[direction](collapse.current_position)
+	directions[direction](collapse.position)
 end
 
 function Public.set_speed(speed)
 	if not speed then print_debug(8) return end	
 	speed = math_floor(speed)
-	if speed < 0 then speed = 0 end
+	if speed < 1 then speed = 1 end
 	collapse.speed = speed	
 end
 
@@ -106,7 +108,7 @@ function Public.set_amount(amount)
 	collapse.amount = amount
 end
 
-function Public.set_start_position(position)
+function Public.set_position(position)
 	if not position then print_debug(4) return end
 	if not position.x and not position[1] then print_debug(5) return end
 	if not position.y and not position[2] then print_debug(6) return end		
@@ -119,6 +121,10 @@ function Public.set_start_position(position)
 	collapse.position = {x = x, y = y}
 end
 
+function Public.get_position()
+	return collapse.position
+end
+
 function Public.set_max_line_size(size)
 	if not size then print_debug(22) return end	
 	size = math_floor(size)
@@ -126,19 +132,19 @@ function Public.set_max_line_size(size)
 	collapse.max_line_size = size
 end
 
-function Public.reset()
-	if not collapse.surface then Public.set_surface(game.surfaces.nauvis) end
-	if not collapse.position then Public.set_start_position({0, 32}) end
-	collapse.current_position = {x = collapse.position.x, y = collapse.position.y}
-	if not collapse.max_line_size then Public.set_max_line_size(256) end
-	if not collapse.vector then Public.set_direction("north") end
-	collapse.tiles = nil
-	collapse.speed = 1
-	collapse.amount = 8
+function Public.set_kill_entities(a)
+	collapse.kill = a
 end
 
 local function on_init()
-	Public.reset()
+	Public.set_surface(game.surfaces.nauvis)
+	Public.set_position({0, 32})
+	Public.set_max_line_size(256)
+	Public.set_direction("north")
+	Public.set_kill_entities(true)
+	collapse.tiles = nil
+	collapse.speed = 1
+	collapse.amount = 8
 end
 
 local function on_tick()
