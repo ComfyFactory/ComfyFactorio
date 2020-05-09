f8e8f27ef3369647170cdd67ce931f4d5837d93f
Gerkiz <fs@nvfs.se>
2020-05-10 00:34:22 +0200

    minor changes

--- modules/burden.lua
+++ modules/burden.lua
@@ -47,9 +47,7 @@ local function check_burden(event)
     player_modifiers[player.index].character_running_speed_modifier['randomness'] = 0.3 - fullness
     player_modifiers[player.index].character_mining_speed_modifier['randomess'] = 0.3 - fullness
     Modifier.update_player_modifiers(player)
-    if fullness >= 0.3 and fullness <= 0.305 then
-        player.print('You feel all of a sudden burden.', Color.yellow)
-    elseif fullness >= 0.5 and fullness <= 0.505 then
+    if fullness >= 0.5 and fullness <= 0.501 then
         player.print('Maybe you should drop some of that inventory to lessen the burden.', Color.red)
     end
 end
