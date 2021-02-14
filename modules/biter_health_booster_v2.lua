921be447b73ed357a77fd429316dfbdce01248ea
Gerkiz <fs@nvfs.se>
2021-02-14 21:33:46 +0100

    changes to biter health booster v2

--- modules/biter_health_booster_v2.lua
+++ modules/biter_health_booster_v2.lua
@@ -3,12 +3,17 @@
 -- Use this.biter_health_boost or this.biter_health_boost_forces to modify their health.
 -- 1 = vanilla health, 2 = 200% vanilla health
 -- do not use values below 1
+
 local Event = require 'utils.event'
+local LootDrop = require 'modules.mobs_drop_loot'
+local WD = require 'modules.wave_defense.table'
 local Global = require 'utils.global'
 
 local floor = math.floor
 local insert = table.insert
 local round = math.round
+local random = math.random
+local sqrt = math.sqrt
 local Public = {}
 
 local this = {
@@ -16,7 +21,42 @@ local this = {
     biter_health_boost_forces = {},
     biter_health_boost_units = {},
     biter_health_boost_count = 0,
-    active_surface = 'nauvis'
+    active_surface = 'nauvis',
+    acid_lines_delay = {},
+    acid_nova = false,
+    boss_spawns_projectiles = false,
+    enable_boss_loot = false
+}
+
+local radius = 6
+local targets = {}
+local acid_splashes = {
+    ['big-biter'] = 'acid-stream-worm-medium',
+    ['behemoth-biter'] = 'acid-stream-worm-big'
+}
+local acid_lines = {
+    ['big-spitter'] = 'acid-stream-spitter-big',
+    ['behemoth-spitter'] = 'acid-stream-spitter-big'
+}
+for x = radius * -1, radius, 1 do
+    for y = radius * -1, radius, 1 do
+        if sqrt(x ^ 2 + y ^ 2) <= radius then
+            targets[#targets + 1] = {x = x, y = y}
+        end
+    end
+end
+
+local projectiles = {
+    'slowdown-capsule',
+    'defender-capsule',
+    'destroyer-capsule',
+    'laser',
+    'distractor-capsule',
+    'rocket',
+    'explosive-rocket',
+    'grenade',
+    'rocket',
+    'grenade'
 }
 
 Global.register(
@@ -33,6 +73,10 @@ function Public.reset_table()
     this.biter_health_boost_count = 0
     this.active_surface = 'nauvis'
     this.check_on_entity_died = false
+    this.acid_lines_delay = {}
+    this.acid_nova = false
+    this.boss_spawns_projectiles = false
+    this.enable_boss_loot = false
 end
 
 local entity_types = {
@@ -43,6 +87,76 @@ local entity_types = {
 
 if is_loaded('maps.biter_hatchery.terrain') then
     entity_types['unit-spawner'] = nil
+end
+
+local function loaded_biters(event)
+    local cause = event.cause
+    local entity = event.entity
+    if not entity or not entity.valid then
+        return
+    end
+
+    local position = false
+    if cause then
+        if cause.valid then
+            position = cause.position
+        end
+    end
+    if not position then
+        position = {entity.position.x + (-20 + random(0, 40)), entity.position.y + (-20 + random(0, 40))}
+    end
+
+    entity.surface.create_entity(
+        {
+            name = projectiles[random(1, 10)],
+            position = entity.position,
+            force = 'neutral',
+            source = entity.position,
+            target = position,
+            max_range = 16,
+            speed = 0.01
+        }
+    )
+end
+
+local function acid_nova(event)
+    for _ = 1, random(20, 40) do
+        local i = random(1, #targets)
+        event.entity.surface.create_entity(
+            {
+                name = acid_splashes[event.entity.name],
+                position = event.entity.position,
+                force = event.entity.force.name,
+                source = event.entity.position,
+                target = {x = event.entity.position.x + targets[i].x, y = event.entity.position.y + targets[i].y},
+                max_range = radius,
+                speed = 0.001
+            }
+        )
+    end
+end
+local function acid_line(surface, name, source, target)
+    local distance = sqrt((source.x - target.x) ^ 2 + (source.y - target.y) ^ 2)
+    local modifier = {(target.x - source.x) / distance, (target.y - source.y) / distance}
+
+    local position = {source.x, source.y}
+
+    for i = 1, distance * 1.5, 1 do
+        if random(1, 2) ~= 1 then
+            surface.create_entity(
+                {
+                    name = name,
+                    position = source,
+                    force = 'enemy',
+                    source = source,
+                    target = position,
+                    max_range = 25,
+                    speed = 1
+                }
+            )
+        end
+        position = {position[1] + modifier[1], position[2] + modifier[2]}
+    end
 end
 
 local function clean_table()
@@ -131,16 +245,38 @@ local function on_entity_damaged(event)
     if not (biter and biter.valid) then
         return
     end
-    if not entity_types[biter.type] then
+    local cause = event.cause
+    if not cause then
         return
     end
 
     local biter_health_boost_units = this.biter_health_boost_units
 
     local unit_number = biter.unit_number
+    local cause_unit_number = cause.unit_number
 
     --Create new health pool
     local health_pool = biter_health_boost_units[unit_number]
+    local cause_health_pool = biter_health_boost_units[cause_unit_number]
+
+    if cause_health_pool and cause_health_pool[3] and entity_types[cause.type] then
+        if this.acid_nova then
+            if acid_lines[cause.name] then
+                if not this.acid_lines_delay[cause_unit_number] then
+                    this.acid_lines_delay[cause_unit_number] = 0
+                end
+                if this.acid_lines_delay[cause_unit_number] < game.tick then
+                    acid_line(cause.surface, acid_lines[cause.name], cause.position, event.entity.position)
+                    this.acid_lines_delay[cause_unit_number] = game.tick + 180
+                end
+            end
+        end
+    end
+
+    if not entity_types[biter.type] then
+        return
+    end
+
     if not health_pool then
         if this.biter_health_boost_forces[biter.force.index] then
             Public.add_unit(biter, this.biter_health_boost_forces[biter.force.index])
@@ -196,9 +332,29 @@ local function on_entity_died(event)
 
     local unit_number = biter.unit_number
 
+    local wave_count = WD.get_wave()
+
     local health_pool = biter_health_boost_units[unit_number]
-    if health_pool then
+    if health_pool and health_pool[3] then
+        if this.enable_boss_loot then
+            if random(1, 128) == 1 then
+                LootDrop.drop_loot(biter, wave_count)
+            end
+        end
+        if this.boss_spawns_projectiles then
+            if random(1, 96) == 1 then
+                loaded_biters(event)
+            end
+        end
         biter_health_boost_units[unit_number] = nil
+        if this.acid_nova then
+            if acid_splashes[biter.name] then
+                acid_nova(event)
+            end
+            if this.acid_lines_delay[biter.unit_number] then
+                this.acid_lines_delay[biter.unit_number] = nil
+            end
+        end
     end
 end
 
@@ -228,10 +384,27 @@ function Public.set_active_surface(str)
     return this.active_surface
 end
 
+function Public.acid_nova(value)
+    this.acid_nova = value or false
+    return this.acid_nova
+end
+
 function Public.check_on_entity_died(boolean)
     this.check_on_entity_died = boolean or false
 
     return this.check_on_entity_died
+end
+
+function Public.boss_spawns_projectiles(boolean)
+    this.boss_spawns_projectiles = boolean or false
+
+    return this.boss_spawns_projectiles
+end
+
+function Public.enable_boss_loot(boolean)
+    this.enable_boss_loot = boolean or false
+
+    return this.enable_boss_loot
 end
 
 local on_init = function()
