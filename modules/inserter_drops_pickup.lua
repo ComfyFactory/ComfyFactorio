f088fd215c5321929cfe48cb478df597398a7f45
Jacob <jacobwatkins2011+github@gmail.com>
2021-11-09 00:28:49 -0500

    inserter_drops_pickup player must have inventory
    event may error if player does not have an inventory

--- modules/inserter_drops_pickup.lua
+++ modules/inserter_drops_pickup.lua
@@ -1,4 +1,9 @@
 local Event = require 'utils.event'
+local controllers_with_inventory = {
+	[defines.controllers.character] = true,
+	[defines.controllers.god] = true,
+	[defines.controllers.editor] = true,
+}
 
 Event.add(defines.events.on_player_mined_entity, function(event)
 	local inserter = event.entity
@@ -6,6 +11,9 @@ Event.add(defines.events.on_player_mined_entity, function(event)
 
 	local item_entity = inserter.surface.find_entity("item-on-ground", inserter.drop_position)
 	if item_entity then
-		game.get_player(event.player_index).mine_entity(item_entity)
+		local player = game.get_player(event.player_index)
+		if controllers_with_inventory[player.controller_type] then
+			player.mine_entity(item_entity)
+		end
 	end
 end)
