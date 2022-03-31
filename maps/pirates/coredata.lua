ae02f762df66741b9719640006aa254c93ea40d1
danielmartin0 <danielmartin111@gmail.com>
2022-03-31 13:11:30 +0100

    typo v1.1.1.5.4

--- maps/pirates/coredata.lua
+++ maps/pirates/coredata.lua
@@ -5,8 +5,8 @@ local _inspect = require 'utils.inspect'.inspect
 local Public = {}
 
 Public.scenario_id_name = 'pirates'
-Public.version_string = '1.1.1.5.3'
-Public.version_float = 1.1153
+Public.version_string = '1.1.1.5.4'
+Public.version_float = 1.1154
 
 Public.blueprint_library_allowed = true
 Public.blueprint_importing_allowed = true
