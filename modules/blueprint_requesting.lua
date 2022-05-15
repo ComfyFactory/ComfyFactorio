50469c804bcc1e26855c05f9c737bc436a7b455e
hanakocz <hanakocz@users.noreply.github.com>
2022-05-16 01:02:25 +0200

    bp requesting - player can disable the feature in config menu

--- modules/blueprint_requesting.lua
+++ modules/blueprint_requesting.lua
@@ -1,10 +1,29 @@
 --module by Hanakocz
 local Event = require 'utils.event'
+local Global = require 'utils.global'
+local Public = {}
+
+local this = {}
+
+Global.register(
+    this,
+    function(tbl)
+        this = tbl
+    end
+)
 
 local function on_gui_closed(event)
+    local player_index = event.player_index
+    if this[player_index] and this[player_index].disabled then return end -- player disabled usage of this module in config tab for his actions
     local entity = event.entity
     if not entity or not entity.valid then return end
     if entity.name == 'logistic-chest-requester' or entity.name == 'logistic-chest-buffer' then
+        if not this[player_index] or not this[player_index].notified then
+            local player = game.get_player(player_index)
+            player.print({'modules.blueprint_requesting_notify'}, {r= 0.88, g = 0.66, b = 0.02})
+            this[player_index] = this[player_index] or {}
+            this[player_index].notified = true
+        end
         local inventory = entity.get_inventory(defines.inventory.chest)
         if not inventory or not inventory.valid then return end
         if inventory.get_item_count('blueprint') > 0 then
@@ -31,4 +50,14 @@ local function on_gui_closed(event)
     end
 end
 
+function Public.get(key)
+    if key then
+        return this[key]
+    else
+        return this
+    end
+end
+
 Event.add(defines.events.on_gui_closed, on_gui_closed)
+
+return Public
