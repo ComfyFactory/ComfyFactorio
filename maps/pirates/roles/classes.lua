2c5e4fb46a0f44f8882acebbb63ca9df74f0f0de
danielmartin0 <danielmartin111@gmail.com>
2022-06-03 21:53:01 +0100

    fix: locale

--- maps/pirates/roles/classes.lua
+++ maps/pirates/roles/classes.lua
@@ -281,7 +281,7 @@ end
 
 function Public.class_ore_grant(player, how_much, enable_scaling)
 	local count = Public.ore_grant_amount(how_much, enable_scaling)
-	
+
 	if Math.random(4) == 1 then
 		Common.flying_text_small(player.surface, player.position, '[color=0.85,0.58,0.37]+' .. count .. '[/color]')
 		Common.give_items_to_crew{{name = 'copper-ore', count = count}}
