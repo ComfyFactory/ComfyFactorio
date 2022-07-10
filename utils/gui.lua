1f0a6577a3a5955b7e905550d1e3d231e6845dbb
Gerkiz <fs@nvfs.se>
2022-07-11 00:14:21 +0200

    minor changes

--- utils/gui.lua
+++ utils/gui.lua
@@ -397,7 +397,7 @@ end
 
 --- This adds the given gui to the top gui.
 ---@param player userdata
----@param frame userdata
+---@param frame userdata|table
 function Public.add_mod_button(player, frame)
     if Public.get_button_flow(player)[frame.name] and Public.get_button_flow(player)[frame.name].valid then
         return
