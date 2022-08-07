65908c53314b24fad36eb8eb42f823f644de38d2
hanakocz <hanakocz@users.noreply.github.com>
2022-08-08 00:08:04 +0200

    Backpack - fixed the collision with toolbelt tech not accounted

--- modules/backpack_research.lua
+++ modules/backpack_research.lua
@@ -3,7 +3,10 @@
 local Event = require 'utils.event'
 
 local function on_research_finished(event)
-    event.research.force.character_inventory_slots_bonus = game.forces.player.mining_drill_productivity_bonus * 100
+    local force = event.research.force
+    local toolbelt_bonus = (force.technologies['toolbelt'].researched and 10) or 0
+    local prod_bonus = force.mining_drill_productivity_bonus * 100
+    force.character_inventory_slots_bonus = prod_bonus + toolbelt_bonus
 end
 
 Event.add(defines.events.on_research_finished, on_research_finished)
