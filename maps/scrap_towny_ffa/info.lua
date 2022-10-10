763bb2095e0ced59a44485cd7fddec595bf5e427
blubFisch <blubfisch@any.mx>
2022-10-10 12:09:05 +0100

    More info

--- maps/scrap_towny_ffa/info.lua
+++ maps/scrap_towny_ffa/info.lua
@@ -44,7 +44,7 @@ Survive as long as you can. Raid other towns. Defend your town.
 - It's possible to automate trading with the town center! How cool is that?!! Try it out.
     Tip: use filter inserters with to get coins/iron/.. out of the market
 
-- Fishes procreate near towns (the more fishes, the quicker they multiply)
+- Fishes procreate near towns. The more fishes, the quicker they multiply. Automated fish farm, anyone?
 
 - Use /rename-town (chat command) to rename your town
 
