fc26616246add8887fff8b87d9cbe26e33aeb36a
blubFisch <blubfisch@any.mx>
2022-10-12 09:03:35 +0100

    More town fall debugs

--- maps/scrap_towny_ffa/team.lua
+++ maps/scrap_towny_ffa/team.lua
@@ -823,7 +823,7 @@ local function kill_force(force_name, cause)
     if is_suicide then
         message = town_name .. ' has given up'
     elseif cause == nil or not cause.valid or cause.force == nil then
-        message = town_name .. ' has fallen to an unknown entity (FIXME ID0)!' -- TODO: remove after some testing
+        message = town_name .. ' has fallen to an unknown entity (DEBUG ID 0)!' -- TODO: remove after some testing
     elseif cause.force.name == 'player' or cause.force.name == 'rogue' then
         local items = {name = 'coin', count = balance}
         town_center.coin_balance = 0
@@ -842,7 +842,7 @@ local function kill_force(force_name, cause)
         elseif cause.force.name == 'rogue' then
             message = town_name .. ' has fallen to rogues!'
         else
-            message = town_name .. ' has fallen to an unknown entity (FIXME ID1)!' -- TODO: remove after some testing
+            message = town_name .. ' has fallen to an unknown entity (DEBUG ID 1)!' -- TODO: remove after some testing
         end
     elseif cause.force.name ~= 'enemy' then
         if this.town_centers[cause.force.name] ~= nil then
@@ -857,7 +857,8 @@ local function kill_force(force_name, cause)
                 message = town_name .. ' has fallen to ' .. killer_town_center.town_name .. '!'
             end
         else
-            message = town_name .. ' has fallen to an unknown entity (FIXME ID2)!' -- TODO: remove after some testing
+            message = town_name .. ' has fallen to an unknown entity (DEBUG ID 2)!' -- TODO: remove after some testing
+            log("cause.force.name=" .. cause.force.name)
         end
     else
         message = town_name .. ' has fallen to the biters!'
