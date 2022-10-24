56befa681de67eba8301051894ccb0d380541d14
hanakocz <hanakocz@users.noreply.github.com>
2022-10-24 19:00:48 +0200

    landmine module changes, towny optimization for mining, small fixes

--- maps/scrap_towny_ffa/scrap.lua
+++ maps/scrap_towny_ffa/scrap.lua
@@ -2,39 +2,33 @@ local Public = {}
 
 local scrapable = {
     -- simple entity
-    'small-ship-wreck',
-    'medium-ship-wreck',
+    ['small-ship-wreck'] = true,
+    ['medium-ship-wreck'] = true,
     -- simple entity with owner
-    'crash-site-spaceship-wreck-small-1',
-    'crash-site-spaceship-wreck-small-2',
-    'crash-site-spaceship-wreck-small-3',
-    'crash-site-spaceship-wreck-small-4',
-    'crash-site-spaceship-wreck-small-5',
-    'crash-site-spaceship-wreck-small-6',
-    'big-ship-wreck-1',
-    'big-ship-wreck-2',
-    'big-ship-wreck-3',
-    'crash-site-chest-1',
-    'crash-site-chest-2',
-    'crash-site-spaceship-wreck-medium-1',
-    'crash-site-spaceship-wreck-medium-2',
-    'crash-site-spaceship-wreck-medium-3',
-    'crash-site-spaceship-wreck-big-1',
-    'crash-site-spaceship-wreck-big-2',
-    'crash-site-spaceship'
+    ['crash-site-spaceship-wreck-small-1'] = true,
+    ['crash-site-spaceship-wreck-small-2'] = true,
+    ['crash-site-spaceship-wreck-small-3'] = true,
+    ['crash-site-spaceship-wreck-small-4'] = true,
+    ['crash-site-spaceship-wreck-small-5'] = true,
+    ['crash-site-spaceship-wreck-small-6'] = true,
+    ['big-ship-wreck-1'] = true,
+    ['big-ship-wreck-2'] = true,
+    ['big-ship-wreck-3'] = true,
+    ['crash-site-chest-1'] = true,
+    ['crash-site-chest-2'] = true,
+    ['crash-site-spaceship-wreck-medium-1'] = true,
+    ['crash-site-spaceship-wreck-medium-2'] = true,
+    ['crash-site-spaceship-wreck-medium-3'] = true,
+    ['crash-site-spaceship-wreck-big-1'] = true,
+    ['crash-site-spaceship-wreck-big-2'] = true,
+    ['crash-site-spaceship'] = true
 }
 
 function Public.is_scrap(entity)
     if not entity.valid then
         return false
     end
-    local f = false
-    for i = 1, #scrapable, 1 do
-        if entity.name == scrapable[i] then
-            f = true
-        end
-    end
-    return f
+    return scrapable[entity.name] or false
 end
 
 return Public
