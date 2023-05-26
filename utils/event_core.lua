fa5553060afbc05251ef8a06ac096e3ab1d1dcc8
Gerkiz <fs@nvfs.se>
2023-05-26 16:22:14 +0200

    Event - remove unused args

--- utils/event_core.lua
+++ utils/event_core.lua
@@ -12,15 +12,6 @@ local event_handlers = {}
 -- map of nth_tick to handlers[]
 local on_nth_tick_event_handlers = {}
 
---[[ local interface = {
-    get_handler = function()
-        return event_handlers
-    end
-}
-
-if not remote.interfaces['interface'] then
-    remote.add_interface('interface', interface)
-end ]]
 local xpcall = xpcall
 local trace = debug.traceback
 local log = log
@@ -33,13 +24,11 @@ local function handler_error(err)
 end
 
 local function call_handlers(handlers, event)
-    if _DEBUG then
-        for i = 1, #handlers do
+    for i = 1, #handlers do
+        if _DEBUG then
             local handler = handlers[i]
             handler(event)
-        end
-    else
-        for i = 1, #handlers do
+        else
             xpcall(handlers[i], handler_error, event)
         end
     end
@@ -91,23 +80,16 @@ local function on_nth_tick_event(event)
 end
 
 --- Do not use this function, use Event.add instead as it has safety checks.
-function Public.add(event_name, handler, filters)
+function Public.add(event_name, handler)
     local handlers = event_handlers[event_name]
+
     if not handlers then
         event_handlers[event_name] = {handler}
-        if filters then
-            script_on_event(event_name, on_event, filters)
-        else
-            script_on_event(event_name, on_event)
-        end
+        script_on_event(event_name, on_event)
     else
         table.insert(handlers, handler)
         if #handlers == 1 then
-            if filters then
-                script_on_event(event_name, on_event, filters)
-            else
-                script_on_event(event_name, on_event)
-            end
+            script_on_event(event_name, on_event)
         end
     end
 end
