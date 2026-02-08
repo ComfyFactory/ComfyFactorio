cb4b811f0e73451f7ac433142de8d089357bc51a
Gerkiz <fs@nvfs.se>
2026-02-08 01:07:31 +0100

    Mtn v3: fix function declaration

--- maps/mountain_fortress_v3/functions.lua
+++ maps/mountain_fortress_v3/functions.lua
@@ -669,9 +669,10 @@ local function do_replace_tiles_slowly()
 end
 
 
--- local function do_custom_surface_funcs()
--- Task.set_timeout_in_ticks(30, custom_surface_funcs_token)
--- end
+local function do_custom_surface_funcs()
+    -- do nothing
+    -- Task.set_timeout_in_ticks(30, custom_surface_funcs_token)
+end
 
 local function do_season_fix()
     local active_surface_index = Public.get('active_surface_index')
@@ -3570,7 +3571,7 @@ Event.on_nth_tick(25, do_beams_away)
 Event.on_nth_tick(30, do_clear_enemy_spawners)
 Event.on_nth_tick(35, do_clear_rocks_slowly)
 Event.on_nth_tick(35, do_replace_tiles_slowly)
--- Event.on_nth_tick(200, do_custom_surface_funcs)
+Event.on_nth_tick(200, do_custom_surface_funcs)
 Event.on_nth_tick(60, set_difficulty)
 Event.add(CustomEvents.events.on_wave_created, on_wave_created)
 Event.add(CustomEvents.events.on_primary_target_missing, on_primary_target_missing)
