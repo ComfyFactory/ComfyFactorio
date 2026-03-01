f55da86e4c88b86ab9b3122c8c20745974a05dc6
Gerkiz <fs@nvfs.se>
2026-03-01 22:38:05 +0100

    Fix templates

---
+++ utils/templates/SpaceAge/map_loader.lua
@@ -0,0 +1,19 @@
+require 'utils.comfy_logo'
+require 'modules.map_info'
+require 'utils.freeplay'
+local Autostash = require 'modules.autostash'
+local Misc = require 'utils.commands.misc'
+local Gui = require 'utils.gui'
+local Event = require 'utils.event'
+
+Gui.mod_gui_button_enabled = true
+Gui.button_style = 'mod_gui_button'
+Gui.set_mod_gui_top_frame(true)
+
+Event.on_init(
+    function ()
+        Autostash.set_enabled(true)
+        Autostash.insert_into_furnace(true)
+        Misc.set_enabled(true)
+    end
+)
