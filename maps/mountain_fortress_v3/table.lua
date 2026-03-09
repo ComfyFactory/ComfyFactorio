10b9b3a68605d45868230ef922b98c0fa2d97887
Gerkiz <fs@nvfs.se>
2026-03-09 01:18:00 +0100

    Mtn v3: minor change

--- maps/mountain_fortress_v3/table.lua
+++ maps/mountain_fortress_v3/table.lua
@@ -11,7 +11,7 @@ local CustomEvents = require 'utils.created_events'
 local stateful_settings =
 {
     reversed = false,
-    current_planet = 'nauvis',
+    current_planet = 'fortress',
     darkness = false
 }
 
